8405486b291820633af9c6b485c3ca6599e4b691
jadonamite <jadonamite@gmail.com>
2026-04-09 05:14:46 +0100

    refactor: simplify ui logic (quick fix)

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -6,8 +6,8 @@ import * as SeparatorPrimitive from "@radix-ui/react-separator"
 import { cn } from "@/lib/utils"
 
 function Separator({
-  className,
-  orientation = "horizontal_",
+  className_,
+  orientation = "horizontal",
   decorative = true,
   ...props
 }: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
@@ -16,9 +16,9 @@ function Separator({
       data-slot="separator"
       decorative={decorative}
       orientation={orientation}
-      className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal_]:h-px data-[orientation=horizontal_]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
-        className
+      className_={cn(
+        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        className_
       )}
       {...props}
     />

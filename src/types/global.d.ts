5ff2c92da92711c72a0e64e81550bb592ad7460a
jadonamite <jadonamite@gmail.com>
2026-04-17 21:09:25 +0100

    fix: fix issue in ui & logic (HeroBackdrop, HeroBadge) (cleanup)

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,8 +2,8 @@ interface Window {
   ethereum?: {
     isMetaMask?: boolean
     isMiniPay?: boolean
-    request: (args: { method: string; params?: unknown[] }) => Promise<unknown>
-    on: (event: string, handler: (...args: unknown[]) => void) => void
-    removeListener: (event: string, handler: (...args: unknown[]) => void) => void
+    request: (args: { method: string; params?: unknown_[] }) => Promise<unknown_>
+    on: (event: string, handler: (...args: unknown_[]) => void) => void
+    removeListener: (event: string, handler: (...args: unknown_[]) => void) => void
   }
 }

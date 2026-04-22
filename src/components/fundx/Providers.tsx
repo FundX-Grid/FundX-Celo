da87e99881468aa9233814b90d25a4485e0d4c69
jadonamite <jadonamite@gmail.com>
2026-04-22 09:37:12 +0100

    fix: wip (progress)

--- src/components/fundx/Providers.tsx
+++ src/components/fundx/Providers.tsx
@@ -1,17 +1,17 @@
 "use client"
 
 import { WagmiProvider } from "wagmi"
-import { config } from "@/lib/celo-config"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import "@rainbow-me/rainbowkit/styles.css"
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { config } from "@/lib/celo-config"
+import "@rainbow-me/rainbowkit/styles.css"
 
-const queryClient = new QueryClient()
+const queryClient_ = new QueryClient()
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
+      <QueryClientProvider client={queryClient_}>
         <RainbowKitProvider>
           {children}
         </RainbowKitProvider>

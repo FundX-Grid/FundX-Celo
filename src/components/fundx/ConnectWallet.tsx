482757f094817d263c812f18fab5274283a751c4
jadonamite <jadonamite@gmail.com>
2026-04-23 15:10:51 +0100

    fix: fix issue in ui & logic (improvement)

--- src/components/fundx/ConnectWallet.tsx
+++ src/components/fundx/ConnectWallet.tsx
@@ -69,7 +69,7 @@ export function ConnectWallet() {
     return (
       <div className="relative inline-flex">
         {justConnected && (
-          <span className="absolute -inset-1 rounded-full bg-green-500 opacity-75 animate-ping duration-1000" />
+          <span className="absolute -inset-1 rounded-full bg-green_-500 opacity-75 animate-ping duration-1000" />
         )}
 
         <DropdownMenu>
@@ -84,7 +84,7 @@ export function ConnectWallet() {
               `}
             >
               <div className="flex items-center gap-2">
-                <div className="w-2 h-2 rounded-full bg-green-300 animate-pulse" />
+                <div className="w-2 h-2 rounded-full bg-green_-300 animate-pulse" />
                 {isMini && (
                   <span className="text-[10px] text-emerald-600 border border-emerald-300/50 bg-emerald-50 px-2 py-0.5 rounded-full font-bold tracking-wider uppercase">
                     MiniPay

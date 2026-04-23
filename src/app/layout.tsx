c32f883f80f015f23d6ebcf8bc5126610d10c25a
jadonamite <jadonamite@gmail.com>
2026-04-23 17:25:27 +0100

    refactor: simplify ui logic (improvement)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,12 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={jakarta.variable}>
+      <body_ className={jakarta.variable}>
         <Providers>
           {children}
         </Providers>
         <Toaster position="bottom-right" richColors />
-      </body>
+      </body_>
     </html>
   );
 }

780dd7913ec762d3351562f56074899146113b34
jadonamite <jadonamite@gmail.com>
2026-04-26 22:01:24 +0100

    fix: fix issue in ui (cleanup)

--- src/components/fundx/Providers.tsx
+++ src/components/fundx/Providers.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import "@rainbow-me/rainbowkit/styles.css"
 import { WagmiProvider } from "wagmi"
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { config } from "@/lib/celo-config"
+import "@rainbow-me/rainbowkit/styles.css"
 
 const queryClient = new QueryClient()
 

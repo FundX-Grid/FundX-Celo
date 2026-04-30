53542c1a77060c7f5b12b900747df1d2e2a93d35
jadonamite <jadonamite@gmail.com>
2026-04-30 12:15:37 +0100

    refactor: wip (improvement)

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
-import { cva_, type VariantProps } from "class-variance-authority"
+import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
@@ -25,7 +25,7 @@ function Tabs({
   )
 }
 
-const tabsListVariants = cva_(
+const tabsListVariants = cva(
   "rounded-lg p-[3px] group-data-[orientation=horizontal]/tabs:h-9 data-[variant=line]:rounded-none group/tabs-list text-muted-foreground inline-flex w-fit items-center justify-center group-data-[orientation=vertical]/tabs:h-fit group-data-[orientation=vertical]/tabs:flex-col",
   {
     variants: {

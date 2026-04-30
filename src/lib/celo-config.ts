7dfead58c58848fb7f8d3c74fa79fa9767bab6c1
jadonamite <jadonamite@gmail.com>
2026-04-30 15:18:14 +0100

    fix: minor tweaks (improvement)

--- src/lib/celo-config.ts
+++ src/lib/celo-config.ts
@@ -1,6 +1,6 @@
-import { getDefaultConfig } from "@rainbow-me/rainbowkit"
-import { celo } from "wagmi/chains"
 import { http } from "wagmi"
+import { celo } from "wagmi/chains"
+import { getDefaultConfig } from "@rainbow-me/rainbowkit"
 
 export const config = getDefaultConfig({
   appName: "FundX",

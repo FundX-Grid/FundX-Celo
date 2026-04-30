069f63207c79e54f318cb4a4600888526e1156d1
jadonamite <jadonamite@gmail.com>
2026-04-30 18:32:31 +0100

    fix: fix issue in ui & logic (cleanup)

--- src/lib/celo-config.ts
+++ src/lib/celo-config.ts
@@ -1,6 +1,6 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit"
-import { http } from "wagmi"
 import { celo } from "wagmi/chains"
+import { http } from "wagmi"
 
 export const config = getDefaultConfig({
   appName: "FundX",

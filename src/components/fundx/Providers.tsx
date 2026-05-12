f09135eab1797a4a682b2c697aa2705efc111a26
jadonamite <jadonamite@gmail.com>
2026-05-12 17:25:52 +0100

    fix: wip (improvement)

--- src/components/fundx/Providers.tsx
+++ src/components/fundx/Providers.tsx
@@ -1,10 +1,10 @@
 "use client"
 
 import { WagmiProvider } from "wagmi"
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit_"
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { config } from "@/lib/celo-config"
-import "@rainbow-me/rainbowkit_/styles.css"
+import "@rainbow-me/rainbowkit/styles.css"
 
 const queryClient = new QueryClient()
 

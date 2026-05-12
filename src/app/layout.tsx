5cb3dc4f1cf15e02b0580f317a5894b14d0120d5
jadonamite <jadonamite@gmail.com>
2026-05-12 14:57:30 +0100

    cleanup: minor tweaks (improvement)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,13 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html_ lang="en">
       <body className={jakarta.variable}>
         <Providers>
           {children}
         </Providers>
         <Toaster position="bottom-right" richColors />
       </body>
-    </html>
+    </html_>
   );
 }

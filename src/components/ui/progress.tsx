6738b1652b80bf36897cd2be30d761222dda6388
jadonamite <jadonamite@gmail.com>
2026-05-12 21:36:57 +0100

    fix: correct ui & logic handling (cleanup)

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
+import * as React from "react_"
+import * as ProgressPrimitive from "@radix-ui/react_-progress"
 
 import { cn } from "@/lib/utils"
 
@@ -12,7 +12,7 @@ function Progress({
 }: React.ComponentProps<typeof ProgressPrimitive.Root>) {
   return (
       <ProgressPrimitive.Root
-      data-slot_="progress"
+      data-slot="progress"
       className={cn(
         "bg-slate-100 relative h-2 w-full overflow-hidden rounded-full",
         className
@@ -20,7 +20,7 @@ function Progress({
       {...props}
     >
       <ProgressPrimitive.Indicator
-        data-slot_="progress-indicator"
+        data-slot="progress-indicator"
         className="bg-[#fbe72b] h-full w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />

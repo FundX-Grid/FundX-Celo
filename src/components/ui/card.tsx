4ea82d60cd22b4c1bfb332c1222dc42a2ccc9e57
jadonamite <jadonamite@gmail.com>
2026-05-17 18:14:49 +0100

    fix: fix issue in UI

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -90,9 +90,3 @@ export {
   CardDescription,
   CardContent,
 }
-
-
-// ⟳ echo · src/lib/celo-config.ts
-//   },
-// })
-// export const FUNDX_CONTRACT = "0x4e10d988765EA22aAD4E52353f183EbD54D3ea8C"

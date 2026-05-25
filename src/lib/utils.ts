f6eebe2173a5fb14fe4fa2eaea264532656ec4c7
jadonamite <jadonamite@gmail.com>
2026-05-25 20:06:05 +0100

    docs: add TODO note for future optimization in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
+// FIXME: handle edge case when value is null
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {

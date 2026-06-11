b509cd9f26892a8c1dabba5370f17f4091fbcb16
jadonamite <jadonamite@gmail.com>
2026-06-11 18:41:41 +0100

    docs: add TODO note for future optimization in page.tsx

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -197,6 +197,7 @@ export default function CreateCampaign() {
               </div>
             </div>
           </div>
+// FIXME: handle edge case when value is null
 
           {/* RIGHT: Preview Module */}
           <div className="hidden lg:block relative">

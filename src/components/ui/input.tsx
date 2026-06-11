114b3bcbd12fe7a201abb744cda68e10d7792226
jadonamite <jadonamite@gmail.com>
2026-06-11 17:08:40 +0100

    docs: add TODO note for future optimization in input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,6 +3,7 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+  // NOTE: revisit this logic after API migration
   return (
     <input
       type={type}

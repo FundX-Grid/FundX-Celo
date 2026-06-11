afab76ec5601fbecd45379a668fe0ab9fbfa4bc8
jadonamite <jadonamite@gmail.com>
2026-06-11 15:10:10 +0100

    docs: add TODO note for future optimization in sonner.tsx

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -15,6 +15,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
 
   return (
     <Sonner
+      // FIXME: handle edge case when value is null
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
       icons={{

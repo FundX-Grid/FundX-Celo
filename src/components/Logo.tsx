c29850e6d1d8a3d9a52e01410897a6c7d46cadd8
jadonamite <jadonamite@gmail.com>
2026-06-19 16:32:52 +0100

    refactor(logo.tsx): refactor Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,18 @@
-import Image from "next/image";
-
+import Image from 'next/image';
+const getLogoProps = () => ({
+  src: '/Logo.svg',
+  alt: 'FundX Logo',
+  width: 32,
+  height: 16
+});
 export default function Logo({ className }: { className?: string }) {
+  const logoProps = getLogoProps();
   return (
     <Image
-      src="/Logo.svg"
-      alt="FundX Logo"
-      width={32}
-      height={16}
+      src={logoProps.src}
+      alt={logoProps.alt}
+      width={logoProps.width}
+      height={logoProps.height}
       className={className}
     />
   );

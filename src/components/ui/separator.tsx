004a2b8b0931fc6950e7f151bf452a34edb63b27
jadonamite <jadonamite@gmail.com>
2026-06-24 01:26:00 +0100

    feat(components/fundx): updated TrustStrips component

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,22 +1,16 @@
-use client
+"use client"
 import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
 import { cn } from "@/lib/utils"
 
-const getHorizontalClassName = (className?: string) =>
-  cn("h-px w-full", className)
-
-const getVerticalClassName = (className?: string) =>
-  cn("h-full w-px", className)
-
-function getSeparatorClassName(
-  orientation: string,
-  className?: string
-) {
+function getSeparatorClassName(orientation: string, className?: string) {
   return cn(
     "bg-border shrink-0",
-    orientation === "horizontal" ? getHorizontalClassName(className) : getVerticalClassName(className)
-  )}
+    { "data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full": orientation === "horizontal" },
+    { "data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px": orientation === "vertical" },
+    className
+  )
+}
 
 function Separator({
   className,
@@ -32,6 +26,6 @@ function Separator({
       className={getSeparatorClassName(orientation, className)}
       {...props}
     />
-  )}
-
+  )
+}
 export { Separator }

e68c2e88492eb8189d03a131771bc0b1f26aacc9
jadonamite <jadonamite@gmail.com>
2026-06-25 13:17:02 +0100

    refactor(ui): refactor separator

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,31 +1,24 @@
-"use client"
+use client
 import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
 import { cn } from "@/lib/utils"
 
-function getSeparatorClassName(orientation: string, className?: string) {
-  return cn(
-    "bg-border shrink-0",
-    { "data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full": orientation === "horizontal" },
-    { "data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px": orientation === "vertical" },
-    className
-  )
+function getHorizontalClassName(className?: string) {
+  return cn("h-px w-full", className)
 }
 
-function Separator({
-  className,
-  orientation = "horizontal",
-  decorative = true,
-  ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
-  return (
-    <SeparatorPrimitive.Root
-      data-slot="separator"
-      decorative={decorative}
-      orientation={orientation}
-      className={getSeparatorClassName(orientation, className)}
-      {...props}
-    />
-  )
+function getVerticalClassName(className?: string) {
+  return cn("h-full w-px", className)
 }
+
+function getSeparatorClassName(orientation: string, className?: string) {
+  if (orientation === "horizontal") return cn("bg-border shrink-0", getHorizontalClassName(className))
+  if (orientation === "vertical") return cn("bg-border shrink-0", getVerticalClassName(className))
+  return ""
+}
+
+function Separator({ className, orientation = "horizontal", decorative = true, ...props }: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+  return (
+    <SeparatorPrimitive.Root data-slot="separator" decorative={decorative} orientation={orientation} className={getSeparatorClassName(orientation, className)} {...props} />
+  )}
 export { Separator }

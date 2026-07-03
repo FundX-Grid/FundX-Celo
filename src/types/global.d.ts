567d115a24ba9851800d375eec64030431f58a4e
jadonamite <jadonamite@gmail.com>
2026-07-03 04:18:40 +0100

    feat(types): add Ethereum interface to global types.

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,9 +1,11 @@
+interface Ethereum {
+  isMetaMask?: boolean
+  isMiniPay?: boolean
+  request: (args: { method: string; params?: unknown[] }) => Promise<unknown>
+  on: (event: string, handler: (...args: unknown[]) => void) => void
+  removeListener: (event: string, handler: (...args: unknown[]) => void) => void
+}
+
 interface Window {
-  ethereum?: {
-    isMetaMask?: boolean
-    isMiniPay?: boolean
-    request: (args: { method: string; params?: unknown[] }) => Promise<unknown>
-    on: (event: string, handler: (...args: unknown[]) => void) => void
-    removeListener: (event: string, handler: (...args: unknown[]) => void) => void
-  }
+  ethereum?: Ethereum
 }

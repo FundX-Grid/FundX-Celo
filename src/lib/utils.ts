02e43981619a728513d46e699ed812a67d7c5d2c
jadonamite <jadonamite@gmail.com>
2026-07-05 00:39:01 +0100

    feat(utils): add mergeClassNames function.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const mergeClassNames = (classValues: ClassValue[]) => twMerge(clsx(classValues))
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return mergeClassNames(inputs)
 }

9f5308bf245aa650446a61d3ad4585af90989866
jadonamite <jadonamite@gmail.com>
2026-07-05 00:14:12 +0100

    feat(ui): update card component styles and props.

--- src/components/fundx/cards/LeftCard.tsx
+++ src/components/fundx/cards/LeftCard.tsx
@@ -6,7 +6,7 @@ import { CampaignProgress } from "./CampaignProgress"
 
 interface SideCardProps {
   campaign: Campaign
-  progress: number
+  progress: number,
 }
 
 export function LeftCard({ campaign, progress }: SideCardProps) {

a1731a68f5ceffed74c3a22f5e282cd11c370047
jadonamite <jadonamite@gmail.com>
2026-07-05 00:40:22 +0100

    feat(components): add new cards for contribution status.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-const mergeClassNames = (classValues: ClassValue[]) => twMerge(clsx(classValues))
-
 export function cn(...inputs: ClassValue[]) {
-  return mergeClassNames(inputs)
+  return twMerge(clsx(inputs))
 }

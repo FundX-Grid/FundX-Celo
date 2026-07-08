ad6461ae476020e1d452129b90c175f3682886b6
jadonamite <jadonamite@gmail.com>
2026-07-09 00:01:42 +0100

    fix(components/fundx): remove unused handleInputs function and simpli…

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-const handleInputs = (...inputs: ClassValue[]) => clsx(inputs)
-
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(handleInputs(...inputs))
+  return twMerge(clsx(inputs))
 }

e750041f5279ab3f9d2b7ee25d98a24c8047371d
jadonamite <jadonamite@gmail.com>
2026-07-09 03:40:54 +0100

    fix(ui/select): fix popper position class condition

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -75,7 +75,7 @@ function SelectContent({
         <SelectPrimitive.Viewport
           className={cn(
             "p-1",
-            position === "popper" &&
+            position === 'popper' &&
               "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)] scroll-my-1"
           )}
         >
